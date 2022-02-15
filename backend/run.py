39be087e9722037d39ccdc3874a9f96831fe88f8
jiz148 <jizjiz148148@gmail.com>
2022-02-14 23:56:19 -0500

    added run.py for setting system path

--- backend/run.py
+++ backend/run.py
@@ -1,1 +1,10 @@
-pass
+# add system path
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from backend_app.api import app
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
