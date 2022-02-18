c38838b87ed59d260ad7062fd98cf93d0889d8ab
jiz148 <jizjiz148148@gmail.com>
2022-02-17 20:27:38 -0500

    CORS

--- backend_sqlalchemy/backend_app/db.py
+++ backend_sqlalchemy/backend_app/db.py
@@ -2,9 +2,14 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+
+# CORS
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/WinNBQ.db3'
 app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
 db = SQLAlchemy(app)
