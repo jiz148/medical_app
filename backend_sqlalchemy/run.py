af64f1526875603d0a82aab3e80dab8e62ab49c8
Kavit Shah <kas672@scarletmail.rutgers.edu>
2022-02-22 15:10:34 -0500

    changing run.py

--- backend_sqlalchemy/run.py
+++ backend_sqlalchemy/run.py
@@ -1,8 +1,10 @@
 # add system path
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
+backend_path = os.path.dirname(os.path.abspath(__file__))
+project_path = os.path.dirname(backend_path)
+sys.path.append(backend_path)
+sys.path.append(project_path)
 from backend_sqlalchemy.backend_app.api import app
 
 
