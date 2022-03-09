2a3a405cca02d9a0f17511da2b0f5684195d5544
kenerickson00 <ken_erickson@yahoo.com>
2022-03-09 12:03:57 -0500

    fix login styling

--- frontend-vue-cli/src/components/LoginPage.vue
+++ frontend-vue-cli/src/components/LoginPage.vue
@@ -26,8 +26,7 @@
         </div>
         <button id="submitBut" class="btn btn-primary btn-lg" type="submit" @click="loginInfo">Login</button>
         <br>
-        <button id="registerBut" class="btn btn-outline-primary" type="submit" @click="godisclaimer" >Register</button>
-        <br>
+        <button id="registerBut" class="btn btn-link btn-sm" type="submit" @click="godisclaimer" >Create Account</button>
         <button id="forgetBut" class="btn btn-link btn-sm" type="submit" @click="forgotpass">Forgot Password?</button>
     </div>
   </div>
@@ -200,6 +199,8 @@ export default { //controls form input
   }
   #registerBut {
       margin-top: 2em;
+      margin-left: -0.6em;
+      margin-right: 0.4em;
   }
   #forgetBut {
       margin-left: -0.6em;
