be8eac51c7c6bcc5b804f7b062383dd906717e5e
kenerickson00 <ken_erickson@yahoo.com>
2022-03-24 14:55:49 -0400

    create profile page, which is done but needs to be connected to the backend still, and a blank settings page

--- frontend-vue-cli/src/components/SettingsPage.vue
+++ frontend-vue-cli/src/components/SettingsPage.vue
@@ -6,50 +6,14 @@
       </div>
     </div>
     <div id="formElements">
-        <div class="mb-3">
-          <label for="email" class="form-label">Email</label>
-          <input type="email" class="form-control" id="email" v-model="email" placeholder="Enter email">
-      </div>
-        <div class="mb-3">
-            <label for="username" class="form-label">Username</label>
-            <input type="text" class="form-control" id="username" v-model="username" placeholder="Enter username">
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" v-model="password" placeholder="Enter password">
-        </div>
-        <div class="mb-3">
-          <label for="password2" class="form-label">Confirm Password</label>
-          <input type="password" class="form-control" id="password2" v-model="password2" placeholder="Re-type password">
-        </div>
-        <div class="mb-3" id="yeardiv">
-          <label for="year" class="form-label">Birth Year</label>
-          <input type="number" class="form-control" id="year" v-model="year" placeholder="Enter the year you were born">
-       </div>
-       <label class="form-check-label" id="genderlabel" for="gendergroup">Sex</label>
-       <div id="gendergroup">
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="male" id="male" value="male" v-model="gender">
-          <label class="form-check-label" for="male">Male</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="female" id="female" value="female" v-model="gender">
-          <label class="form-check-label" for="female">Female</label>
-        </div>
-       </div>
-       <div class="mb-3">
-        <label for="phone" class="form-label">Phone Number (Optional)</label>
-        <input type="text" class="form-control" id="phone" v-model="phone" placeholder="Enter your phone number">
-      </div>
-        <button id="submitBut" class="btn btn-primary" type="submit" @click="register">Register</button>
-        <button id="backBut" class="btn btn-secondary" type="submit" @click="$router.go(-1)">Cancel</button>
+       <button id="backBut" class="btn btn-secondary" type="submit" @click="$router.go(-1)">Back</button>
     </div>
   </div>
 </template>
 
 <script>
   export default { //controls form input
-    name: "RegisterPage",
+    name: "SettingsPage",
     props: {},
     data() {
         return {
@@ -71,36 +35,41 @@
     beforeCreate: function() {
         let url = "http://127.0.0.1:5001/user/sessiondata";
         fetch(url, { //executes the query with a promise to get around asynchronous javascript behavior
-        method: 'get',
-        credentials: "include",
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json;charset=UTF-8',
-            "Set-Cookie": "test=value; Path=/; Secure; SameSite=None;",
-            'Access-Control-Allow-Origin': '127.0.0.1:5001',
-            'Access-Control-Allow-Credentials': true,
-        }})
-        .then((response) => { 
-            this.status = response.status;
-            return response.json() 
-        })
-        .then(data => {
-          this.response = data; //update table with new data
-          if(this.status == 200) {
-              console.log(this.response);
-              if(this.response.accepted == null) {
-                  this.$router.push('/');
-              }
-          }
-        }).catch(error => {
-        if(error.response) {
+            method: 'get',
+            credentials: "include",
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                "Set-Cookie": "test=value; Path=/; Secure; SameSite=None;",
+                'Access-Control-Allow-Origin': '127.0.0.1:5001',
+                'Access-Control-Allow-Credentials': true,
+            }})
+            .then((response) => { 
+                this.status = response.status;
+                return response.json() 
+            })
+            .then(data => {
+            this.response = data; //update table with new data
             if(this.status == 200) {
-              console.log(this.response.msg); //switch to main page here
+                if(this.response.accepted == null) {
+                    this.$router.push('/');
+                }
+                else if(this.response.uid == null) {
+                    this.$router.push('/login');
+                }
             } else {
-              this.$router.push('/');
+                console.log('bad');
+                this.$router.push('/');
             }
-        }
-        });
+            }).catch(error => {
+            if(error.response) {
+                if(this.status == 200) {
+                console.log(this.response.msg); //switch to main page here
+                } else {
+                this.$router.push('/');
+                }
+            }
+            });
     },
     methods: {
       register() { //keeps track of which database to query
@@ -126,7 +95,7 @@
         } else if(this.password != this.password2) {
             this.errorMess = "The passwords do not match.";
             this.showError = true;
-        } else if(this.year < yyyy-120 || this.year > yyyy) {
+        }  else if(this.year < yyyy-120 || this.year > yyyy) {
             this.errorMess = "Please input a valid year.";
             this.showError = true;
         } else if(yyyy - this.year < 16) {
--- frontend-vue-cli/src/routes.js
+++ frontend-vue-cli/src/routes.js
@@ -5,6 +5,8 @@ import MainPage from './components/MainPage';
 import ForgetPage from './components/ForgetPage';
 import NewPassPage from './components/NewPassPage';
 import VisitPage from './components/VisitPage';
+import ProfilePage from './components/ProfilePage';
+import SettingsPage from './components/SettingsPage';
 
 const routes = [
     {path: '/login', component: LoginPage, props: true},
@@ -13,7 +15,9 @@ const routes = [
     {path: '/visit', component: VisitPage},
     {path: '/main', component: MainPage},
     {path: '/forget', component: ForgetPage},
-    {path: '/newpass', component: NewPassPage, props: true}
+    {path: '/newpass', component: NewPassPage, props: true},
+    {path: '/profile', component: ProfilePage},
+    {path: '/settings', component: SettingsPage}
 ];
 
 export default routes;
