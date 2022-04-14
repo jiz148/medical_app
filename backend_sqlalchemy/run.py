3b4d5124707b30e81177e80b579670c089c02f54
jiz148 <jizjiz148148@gmail.com>
2022-04-14 16:34:38 -0400

    updated run.py

--- backend_sqlalchemy/run.py
+++ backend_sqlalchemy/run.py
@@ -9,4 +9,4 @@ from backend_sqlalchemy.backend_app.api import app
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0,0', debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=True, port=5001)
