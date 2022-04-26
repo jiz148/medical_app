cfd4b77d812e9c919a1442ab3d3d02ee71b4f2b7
jiz148 <jizjiz148148@gmail.com>
2022-04-25 20:36:51 -0400

    updated template

--- backend_sqlalchemy/backend_app/common/contents.py
+++ backend_sqlalchemy/backend_app/common/contents.py
@@ -1,5 +1,5 @@
 FORGET_MY_PASSWORD_SUBJECT = """Password Change Confirmation"""
 FORGET_MY_PASSWORD_CONTENT = """hello {user},\n
                                 Please click the following link to change your password:\n
-                                http://jinchispace.com:5001/newpass/?token={token}\n
+                                http://jinchispace.com:5001/newpass?token={token}\n
                                 This link will be expired in {minutes} minutes."""
