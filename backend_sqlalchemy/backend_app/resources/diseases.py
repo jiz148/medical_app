d70392fbfbbec37570269b1808ed17207c2c9bbd
jiz148 <jizjiz148148@gmail.com>
2022-05-04 01:32:17 -0400

    fixed disease URLs

--- backend_sqlalchemy/backend_app/resources/diseases.py
+++ backend_sqlalchemy/backend_app/resources/diseases.py
@@ -47,7 +47,7 @@ class TopDiseases(Resource):
                 "DID": key,
                 "Name": all_diseases[key]['name'],
                 "cond_p": round(cond_p, 2),
-                "URL": all_diseases[key]['URL']
+                "URL": (all_diseases[key]['URL'] + ';;').split(';')[0]
             })
         disease_results = sorted(disease_results, key=lambda d: d['cond_p'], reverse=True)
         return {'msg': "success", 'data': disease_results[:NUM_OF_RETURNED_DISEASES]}
@@ -78,7 +78,7 @@ def get_all_diseases():
             g_diseases[disease.DID] = {
                 'name': disease.Name,
                 'freq': disease.Frq,
-                'URL': disease.URL
+                'URL': (disease.URL + ';;').split(';')[0]
             }
     return g_diseases
 
